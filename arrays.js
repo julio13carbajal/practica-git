710c32d94d8c6363b0352627b697b9df139c4e6d
julio13carbajal <juliovcarbajal@gmail.com>
2019-01-14 21:35:53 -0600

    Nuevos metodos de areglos

--- arrays.js
+++ arrays.js
@@ -1,2 +1,41 @@
 const number = [1,2,3,4,5,6,7,8,9];
 const names = ['Jay', 'Daniela', ' Pablo'];
+
+
+
+//spread operator (copy values)
+con
+//interacciones
+
+for(let i =0;i<names.length; i++){
+	if(i==1){
+		break;
+	}
+
+	//console.log(names[i]);
+
+}
+
+function logItems(item){
+	console.log(`Name ${item} at index: ${index}`);
+	console.log(arregloOriginal)
+}
+names.forEach(logItems);
+
+const myFunction = function(names){
+	console.log('mario'+names)
+}
+
+//Arrow Function definition
+
+const sum = (a,b) => {
+	const value = a + b;
+	return value;
+}
+//Arrow Function implicit return
+const resta = (a, b) => a - b;
+const fullNames = [];
+const output = names.forEach(item => fullNames.push(item + 'Rodriguez'));
+console.log(output);
+//names.map (item => )
+
